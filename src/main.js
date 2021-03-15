32fa27b85709dc909f937615968f38c7f29694d2
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-15 16:13:53 -0700

    added to main component to contain the hornedbeasts component. added to hornedbeast component to contain h2, img, and p tag information

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import HornedBeasts from './hornedbeasts';
 
 
 class Main extends React.Component{
-
+  render () {
+    return (
+      <div id="main">
+        <HornedBeasts />
+      </div>
+    )
+  }
 }
 
 export default Main;
