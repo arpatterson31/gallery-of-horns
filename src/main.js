f44d5555db6edd4f70128a7f987a76f4b5c4bb1e
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-17 10:35:30 -0700

    corrected map function to correctly enter key

--- src/main.js
+++ src/main.js
@@ -2,15 +2,21 @@ import React from 'react';
 import HornedBeasts from './hornedbeasts';
 import BeastData from './data.json';
 
-class Main extends React.Component{
-  render () {
+class Main extends React.Component {
+  render() {
     return (
       <div id="main">
-        <ul>
-          {BeastData.map((beast) => {
-            return <HornedBeasts url={beast.image_url} title={beast.title} description={beast.description} />
-          })}
-        </ul>
+        {BeastData.map((beast, index) => (
+          <div key={index}>
+            <HornedBeasts
+              url={beast.image_url}
+              title={beast.title}
+              description={beast.description}
+            />
+          </div>
+        ))
+        }
+
       </div>
     );
   }
