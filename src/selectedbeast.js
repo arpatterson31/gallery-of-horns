a13d33d69c56c349e7a4e7398ea87d9afe5d5737
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-17 14:50:51 -0700

    started modal build in selected beast component

--- src/selectedbeast.js
+++ src/selectedbeast.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import Modal from 'react-bootstrap/Modal'
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
-class SelectedBeast extends React.Component{
+class SelectedBeast extends React.Component {
   render() {
     return (
       // render modal
+      <div id="beast-modal">
+        <Modal
+          {...props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              {this.props.title}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img src={this.props.url} alt={this.props.description} title={this.props.title} />
+            <p>{this.props.description}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={props.onHide}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     );
   }
 }
