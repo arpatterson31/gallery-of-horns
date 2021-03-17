defcb28b0b892f2db301176f4e70834d0be146ac
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-03-17 10:37:14 -0700

    added bootstrap styling

--- src/main.js
+++ src/main.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import HornedBeasts from './hornedbeasts';
 import BeastData from './data.json';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CardGroup from 'react-bootstrap/CardGroup';
 
 class Main extends React.Component {
   render() {
     return (
       <div id="main">
-        {BeastData.map((beast, index) => (
-          <div key={index}>
-            <HornedBeasts
-              url={beast.image_url}
-              title={beast.title}
-              description={beast.description}
-            />
-          </div>
-        ))
-        }
-
+        <CardGroup>
+          {BeastData.map((beast, index) => (
+            <div key={index}>
+              <HornedBeasts
+                url={beast.image_url}
+                title={beast.title}
+                description={beast.description}
+              />
+            </div>
+          ))
+          }
+        </CardGroup>
       </div>
     );
   }
